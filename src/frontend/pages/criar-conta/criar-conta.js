cf72d6a16f6894496d563b7cc0b4f551bf37c3c5
Sergio Silva <asteriesc@gmail.com>
2024-06-09 17:36:54 -0300

    refac: refac form inputs

--- src/frontend/pages/criar-conta/criar-conta.js
+++ src/frontend/pages/criar-conta/criar-conta.js
@@ -1,10 +1,12 @@
 const clientForm = document.getElementById('client-form');
 const barberForm = document.getElementById('barber-form');
+const dataForm = document.getElementById('data-form');
 
 clientForm.addEventListener('submit', submitClientForm);
 barberForm.addEventListener('submit', submitBarberForm);
+dataForm.addEventListener('submit', submitDataForm);
 
-async function submitClientForm(event) {
+async function submitDataForm(event){
     event.preventDefault();
 
     const targetForm = event.target;
@@ -17,12 +19,22 @@ async function submitClientForm(event) {
     const endereco = await dados.json();
 
     const name = formData.get('name');
-    const phone = formData.get('telephone');
     const email = formData.get('email');
     const password = formData.get('password');
     const number = formData.get('number');
     const city = endereco.localidade;
     const uf = endereco.uf;
+
+    console.log(endereco);
+}
+
+async function submitClientForm(event) {
+    event.preventDefault();
+
+    const targetForm = event.target;
+    const formData = new FormData(targetForm);
+
+    const phone = formData.get('telephone');
 }
 
 async function submitBarberForm(event) {
@@ -31,24 +43,39 @@ async function submitBarberForm(event) {
     const targetForm = event.target;
     const formData = new FormData(targetForm);
 
-    const cep = formData.get('cep');
-
-    const url = `http://viacep.com.br/ws/${cep}/json/`
-    const dados = await fetch(url);
-    const endereco = await dados.json();
-
     const razao = formData.get('razao');
-    const name = formData.get('name');
     const cnpj = formData.get('cnpj');
-    const phone = formData.get('telephone');
-    const email = formData.get('email');
-    const password = formData.get('password');
-    const number = formData.get('number');
-    const city = endereco.localidade;
-    const uf = endereco.uf;
 }
 
 //Functions
+var currentStep = 1;
+var totalSteps = 2;
+
+function nextStep() {
+    if(dataForm.reportValidity()){
+        if (currentStep < totalSteps) {
+            var currentSection = document.getElementById("step" + currentStep);
+            currentSection.style.display = "none";
+    
+            currentStep++;
+    
+            var nextSection = document.getElementById("step" + currentStep);
+            nextSection.style.display = "flex";
+        }
+    }
+}
+
+function previousStep() {
+    if (currentStep > 1) {
+        var currentSection = document.getElementById("step" + currentStep);
+        currentSection.style.display = "none";
+
+        currentStep--;
+
+        var previousSection = document.getElementById("step" + currentStep);
+        previousSection.style.display = "flex";
+    }
+}
 
 function toggleForm(form, button){
 
